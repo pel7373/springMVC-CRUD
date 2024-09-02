1c4e5771de30f8031aa14707756739602fec33f7
Pavlo Liebiediev <pel7373@gmail.co>
2024-09-03 02:31:26 +0300

    Config, DAO, Service, Controller, html and viewing with paging are done, but edit and delete are not yet.

---
+++ src/main/java/org/example/controller/TaskController.java
@@ -0,0 +1,69 @@
+package org.example.controller;
+
+import org.example.domain.Task;
+import org.example.service.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Objects.isNull;
+
+@Controller
+@RequestMapping("/")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/")
+    public String tasks(Model model,
+                            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                            @RequestParam(value = "limit", required = false, defaultValue = "10") int limit) {
+        List<Task> tasks = taskService.getAll((page - 1) * limit, limit);
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("current_page", page);
+        int totalPages = (int) Math.ceil(1.0 * taskService.getAllCount() / limit);
+        if(totalPages > 1) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+            model.addAttribute("page_numbers", pageNumbers);
+        }
+
+        return "tavisks";
+
+    }
+
+    @PostMapping("/{id}")
+    public String edit(Model model,
+                     @PathVariable Integer id,
+                     @RequestBody TaskDTO taskDTO) {
+        if(isNull(id) || id <= 0) {
+            throw new RuntimeException("Invalid id");
+        }
+
+        Task task = taskService.edit(id, taskDTO.getDescription(), taskDTO.getStatus());
+        return tasks(model, 1, 10);
+    }
+
+    @PostMapping("/")
+    public String add(Model model,
+                     @RequestBody TaskDTO taskDTO) {
+        Task task = taskService.create(taskDTO.getDescription(), taskDTO.getStatus());
+        return tasks(model, 1, 10);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(Model model,
+                       @PathVariable Integer id) {
+        if(isNull(id) || id <= 0) {
+            throw new RuntimeException("Invalid id");
+        }
+        taskService.delete(id);
+        return tasks(model, 1, 10);
+    }
+}
---
+++ src/main/java/org/example/service/TaskService.java
@@ -0,0 +1,68 @@
+package org.example.service;
+
+
+import org.example.dao.TaskDAO;
+import org.example.domain.Status;
+import org.example.domain.Task;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.naming.NamingException;
+import javax.naming.Reference;
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.Objects.isNull;
+
+@Service
+public class TaskService {
+    private final TaskDAO taskDAO;
+
+    public TaskService(TaskDAO taskDAO) {
+        this.taskDAO = taskDAO;
+    }
+
+    public List<Task> getAll(int offset, int limit) {
+        return taskDAO.getAll(offset, limit);
+    }
+
+    public int getAllCount() {
+        return taskDAO.getAllCount();
+    }
+
+    @Transactional
+    public Task edit(int id, String description, Status status) {
+        Task task = taskDAO.getById(id);
+        if(isNull(task)){
+            throw new RuntimeException("Task not found");
+        }
+
+        task.setDescription(description);
+        task.setStatus(status);
+        taskDAO.saveOrUpdate(task);
+        return task;
+    }
+
+    public Task create(String description, Status status) {
+        Task task = new Task();
+        task.setDescription(description);
+        task.setStatus(status);
+        taskDAO.saveOrUpdate(task);
+        return task;
+    }
+
+    @Transactional
+    public void delete(int id) {
+        Task task = taskDAO.getById(id);
+        if(isNull(task)){
+            throw new RuntimeException("Task not found");
+        }
+
+        taskDAO.delete(task);
+    }
+
+
+}
